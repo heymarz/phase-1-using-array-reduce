86d3e33b066a9d028a64b47ae3b395d8c92dd22a
Marlena Tsang <Marlena345@gmail.com>
2021-10-02 16:36:29 -0400

    commit tree 23c03e1717557190a703e05eba4019c5285e1c89

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+//use reduce to initerate array into single value
+
+const totalBatteries = batteryBatches.reduce(function (accumulator, number){return number + accumulator}, 0)
